b8e96257c0885d86c13e8cbe90d526d72938d940
Dayan Perera <dayannp@gmail.com>
2018-02-27 09:26:52 -0800

    Add User model and Passport js Configurations

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
+var sessions = require('express-session');
 var CampGround = require('./models/campground');
 var Comment = require('./models/comment');
-//var User = require('./models/user');
-var seedDB = require("./seeds");
+var User = require('./models/user');
 
 //seedDB();
 //connect or create  mongo db 'camp_grounds'
@@ -14,13 +16,27 @@ mongoose.connect('mongodb://localhost/camp_grounds');
 var port = process.env.PORT || 3000;
 var ip = process.env.IP || '127.0.0.1';
 
-(app.use(bodyParser.urlencoded({extended:true}));)
+(app.use(bodyParser.urlencoded({extended:true})));
 
 //set the path for the static assets
 app.use(express.static(__dirname+"/public"));
 
 //set the view Engine
 app.set("view engine","ejs");
+
+//--- Set up Passport Configurations
+app.use(sessions({
+	secret:"Madola Multi Plantation,Sri Lanka",
+	resave: false,
+	saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//--- End Passport set up ----
 
 app.get('/',function(req,res) {
 	res.render("landing")
@@ -104,6 +120,29 @@ app.post('/camp-grounds/:id/comments/', function (req, res ){
  })
 
 });
+
+/*
+	Authincation Routes
+ */
+
+app.get( '/register',function ( req,res ){
+	res.render('register');
+});
+
+app.post('/register',function( req,res ){
+	var newUser = new User({username:req.body.username});
+	User.register(newUser,req.body.password,function(error ,newUser){
+		if(error) {
+			console.log(error);
+			return res.render('register');
+		}
+		passport.authenticate("local")(req,res, function(){
+			res.redirect('/camp-grounds');
+		});
+	})
+
+});
+//--- End Authincation Routes
 
 app.listen(port,ip,() => {
 	console.log(`Server started listening on port ${port}`);
